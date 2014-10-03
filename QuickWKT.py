455882587d743330e588b4d2e600232e94f43560
elpaso <apasotti@gmail.com>
2014-10-03 12:24:55 +0200

    closes #17 and #15

--- QuickWKT.py
+++ QuickWKT.py
@@ -37,7 +37,7 @@ class QuickWKT:
         # Save reference to the QGIS interface
         self.iface = iface
         self.canvas = iface.mapCanvas()
-
+        # TODO: remove: unused
         self.layerNum = 1
 
     def initGui(self):
@@ -61,6 +61,7 @@ class QuickWKT:
         examples = ""
         self.dlg = QuickWKTDialog()
         self.dlg.wkt.setPlainText(examples)
+        self.dlg.layerTitle.setText('QuickWKT')
 
         #import pdb
         # These lines allow you to set a breakpoint in the app
@@ -90,11 +91,12 @@ class QuickWKT:
         # See if OK was pressed
         if result == 1 and self.dlg.wkt.toPlainText():
             text = unicode(self.dlg.wkt.toPlainText())
+            layerTitle = self.dlg.layerTitle.text() or 'QuickWKT'
             try:
                 if "(" in text:
-                    self.save_wkt(text)
+                    self.save_wkt(text, layerTitle)
                 else:
-                    self.save_wkb(text)
+                    self.save_wkb(text, layerTitle)
             except Exception, e:
                 QMessageBox.information(self.iface.mainWindow(), \
                 QCoreApplication.translate('QuickWKT', "QuickWKT plugin error"), \
@@ -105,14 +107,16 @@ class QuickWKT:
             self.canvas.refresh()
             return
 
-    def createLayer(self, typeString, crs=None):
-        layer = QgsVectorLayer(typeString, "QuickWKT %d" % self.layerNum, "memory")
-        self.layerNum += 1
+    def createLayer(self, typeString, layerTitle, crs=None):
         if crs:
             crs = QgsCoordinateReferenceSystem(crs, QgsCoordinateReferenceSystem.PostgisCrsId)
         else:
             crs = self.canvas.mapRenderer().destinationCrs()
-        layer.setCrs(crs)
+
+        typeString = "%s?crs=%s" % (typeString, crs.authid())
+        layer = QgsVectorLayer(typeString, layerTitle, "memory")
+
+        #layer.setCrs(crs)
         # add attribute id, purely to make the features selectable from within attribute table
         layer.dataProvider().addAttributes([QgsField("name", QVariant.String)])
         QgsMapLayerRegistry.instance().addMapLayer(layer)
@@ -152,7 +156,7 @@ class QuickWKT:
         layer.reload()
         self.canvas.refresh()
 
-    def save_wkb(self, wkb):
+    def save_wkb(self, wkb, layerTitle):
         SRID_FLAG = 0x20000000
 
         typeMap = {0: "Point", 1: "LineString", 2: "Polygon"}
@@ -173,12 +177,12 @@ class QuickWKT:
             qDebug("Geometry creation failed")
         f = QgsFeature()
         f.setGeometry(geom)
-        layer = self.createLayer(typeMap[geom.type()], srid)
+        layer = self.createLayer(typeMap[geom.type()], layerTitle, srid)
 
         self.saveFeatures(layer, [f])
 
     # save wkt to file, wkt is in project's crs
-    def save_wkt(self, wkt):
+    def save_wkt(self, wkt, layerTitle):
         # supported types as needed for layer creation
         typeMap = {0: "Point", 1: "LineString", 2: "Polygon"}
         newFeatures = {}
@@ -189,36 +193,38 @@ class QuickWKT:
         qDebug("wkt: " + wkt);
         # check all lines in text and try to make geometry of it, collecting errors and features
         for wktLine in wkt.split('\n'):
-            try:
-                wktLine = wktLine.upper().replace("LINEARRING", "LINESTRING")
-                results = re.match(regex, wktLine)
-                wktLine = results.group(1) + " " + results.group(2)
-                qDebug("Attempting to save '%s'" % wktLine)
-                #EWKT support
-                srid = ""
-                if wktLine.startswith("SRID"):
-                    srid, wktLine = wktLine.split(";")  # SRID number
-                    srid = int(re.match(".*?(\d+)", srid).group(1))
-                    qDebug("SRID = '%d'" % srid)
+            wktLine = wktLine.strip()
+            if wktLine:
+                try:
+                    wktLine = wktLine.upper().replace("LINEARRING", "LINESTRING")
+                    results = re.match(regex, wktLine)
+                    wktLine = results.group(1) + " " + results.group(2)
+                    qDebug("Attempting to save '%s'" % wktLine)
+                    #EWKT support
+                    srid = ""
+                    if wktLine.startswith("SRID"):
+                        srid, wktLine = wktLine.split(";")  # SRID number
+                        srid = int(re.match(".*?(\d+)", srid).group(1))
+                        qDebug("SRID = '%d'" % srid)
 
-                #Geometry Collections
-                if wktLine.startswith("GEOMETRYCOLLECTION ("):
-                    self.parseGeometryCollection(wktLine)
-                    continue
+                    #Geometry Collections
+                    if wktLine.startswith("GEOMETRYCOLLECTION ("):
+                        self.parseGeometryCollection(wktLine)
+                        continue
 
-                geom = QgsGeometry.fromWkt(wktLine)
-                if not geom:
-                    errors += ('-    "' + wktLine + '" is invalid\n')
-                    continue
+                    geom = QgsGeometry.fromWkt(wktLine)
+                    if not geom:
+                        errors += ('-    "' + wktLine + '" is invalid\n')
+                        continue
 
-                f = QgsFeature()
-                f.setGeometry(geom)
-                if geom.type() in newFeatures:
-                    newFeatures.get(geom.type()).append((f, srid))
-                else:
-                    newFeatures[geom.type()] = [(f, srid)]
-            except:
-                errors += ('-    ' + wktLine + '\n')
+                    f = QgsFeature()
+                    f.setGeometry(geom)
+                    if geom.type() in newFeatures:
+                        newFeatures.get(geom.type()).append((f, srid))
+                    else:
+                        newFeatures[geom.type()] = [(f, srid)]
+                except:
+                    errors += ('-    ' + wktLine + '\n')
         if len(errors) > 0:
             # TODO either quit or succeed ignoring the errors
             infoString = QCoreApplication.translate('QuickWKT', "These line(s) are not WKT or not a supported WKT type:\n" + errors + "Do you want to ignore those lines (OK) \nor Cancel the operation (Cancel)?")
@@ -228,10 +234,12 @@ class QuickWKT:
 
         for typ in newFeatures.keys():
             if self.dlg.cbxnewlayer.isChecked():
-                layer = self.createLayer(typeMap[typ], newFeatures.get(typ)[0][1])
+                # TODO: remove: unused
+                self.layerNum += 1
+                layer = self.createLayer(typeMap[typ], layerTitle, newFeatures.get(typ)[0][1])
             for f in newFeatures.get(typ):
                 if not self.dlg.cbxnewlayer.isChecked():
-                    layer = self.createLayer(typeMap[typ], f[1])
+                    layer = self.createLayer(typeMap[typ], layerTitle , f[1])
                 layer.dataProvider().addFeatures([f[0]])
                 layer.updateExtents()
                 layer.reload()
