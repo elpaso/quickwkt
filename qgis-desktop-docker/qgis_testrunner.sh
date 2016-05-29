2d53f98e21a97d12c6c32a0b99771654c243b065
Alessandro Pasotti <apasotti@boundlessgeo.com>
2016-05-29 18:59:56 +0200

    Fixed exit code

--- qgis-desktop-docker/qgis_testrunner.sh
+++ qgis-desktop-docker/qgis_testrunner.sh
@@ -10,13 +10,14 @@ echo "Running test $1 ..."
 OUTPUT=`unbuffer qgis --nologo --code /usr/bin/qgis_testrunner.py $TEST_NAME  2>/dev/null`
 EXIT_CODE="$?"
 echo $OUTPUT | grep -q FAILED
+TEST_EXIT_CODE="$?"
 if [ -z "$OUTPUT" ]; then
     echo "ERROR: no output from the test runner! (exit code: ${EXIT_CODE})"
     exit 1
 fi
 echo "$OUTPUT"
 echo "Finished running test $1."
-if [ "$EXIT_CODE" != "0" ];
+if [ "$TEST_EXIT_CODE" != "0" ];
     then exit 0;
 fi
 exit 1
