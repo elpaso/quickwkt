2278fa15ef4110b0b90b5bf1ddc87c033de06718
Alessandro Pasotti <apasotti@boundlessgeo.com>
2016-05-31 16:00:41 +0200

    Removed docker folder

--- qgis-desktop-docker/build.sh
+++
@@ -1,9 +0,0 @@
-#Build with localhost address: APT_CATCHER_IP=192.168.99.16
-
-# Start APT Catcher
-# docker run -d --name apt-acng -p 3142:3142 acng:latest
-# docker logs -f apt-acng
-
-ADDR=`ifconfig wlp2s0 | grep 'indirizzo inet:' | cut -d: -f2 | awk '{ print $1}'`
-docker build -t qgis-desktop  --build-arg APT_CATCHER_IP=$ADDR .
-#docker build -t docker-qgis-server-boundless  --build-arg "$@" .
--- qgis-desktop-docker/install.sh
+++
@@ -1,2 +0,0 @@
-# Docker install script, run in the container
-pip install -r /usr/local/requirements.txt
